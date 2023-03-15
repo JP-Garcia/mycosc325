2b55086a44955782a5e6c3581f601b6d472f0017
Brian Toone <brtoone@samford.edu>
2023-03-15 11:39:39 -0500

    added comments to painpanel code

--- java/PaintPanel.java
+++ java/PaintPanel.java
@@ -12,7 +12,6 @@ public class PaintPanel extends JPanel {
     public void paint(Graphics g) {
         super.paint(g);
         System.out.println("painting....");
-        g.setColor(Color.RED);
         int w = this.getWidth();
         int h = this.getHeight();
         int eye = 30;
@@ -23,26 +22,28 @@ public class PaintPanel extends JPanel {
         int nose = 40;
         int mouth = 120;
 
-
+        // draw the face (red)
+        g.setColor(Color.RED);
         g.drawOval(10,10,w-20,h-20);
 
+        // draw the eyes (blue rectangles)
         g.setColor(Color.BLUE);
         g.drawRect(centerx-eyespacing-eye,thirdy-eye,eye,eye);
         g.drawRect(centerx+eyespacing,thirdy-eye,eye,eye);
 
+        // draw the nose (purple triangle)
         g.setColor(Color.MAGENTA);
         g.drawLine(centerx, centery, centerx + nose/2, centery + nose/2);
         g.drawLine(centerx + nose/2, centery + nose/2, centerx - nose/2, centery + nose/2);
         g.drawLine(centerx-nose/2, centery+nose/2, centerx, centery);
 
+        // draw the mouth (black oval ... partially hidden)        
         g.setColor(Color.BLACK);
         g.drawOval(centerx-mouth/2, centery + (int) Math.round(nose*2.5), mouth, nose);
 
+        // hide the top part of the mouth to make it look like a smile
         g.setColor(Color.WHITE);
         g.fillRect(centerx-mouth/2, centery + (int) Math.round(nose*2), mouth, 40);
-
-    }
-
-    
+    }  
 
 }
