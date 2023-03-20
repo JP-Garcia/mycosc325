db55f7cd1419390bf4cec0fa8ae5c650de179cb4
kartoone <brtoone@samford.edu>
2023-03-20 07:28:13 -0500

    Fixed shape example for Graphics shape hierarchy

--- java/ShapeExample.java
+++ java/ShapeExample.java
@@ -1,17 +1,16 @@
 import shapes.*;
 
-import java.awt.Graphics;
 import java.util.ArrayList;
 
 public class ShapeExample {
     public static void main(String[] args) {
         ArrayList<Shape> shapes = new ArrayList<>(); 
-        shapes.add(new Rectangle(25, 5, 0, 0, "red"));
-        shapes.add(new Oval(15, 5, 0, 0, "red"));
-        shapes.add(new Rectangle(8, 5, 0, 0, "red"));
-        shapes.add(new Rectangle(3, 4, 0, 0, "green"));
-        shapes.add(new Square(10, 0, 0, "blue"));
-        shapes.add(new Triangle(1, 1, 4, 2, 3, 3, "yellow"));
+        shapes.add(new Rectangle(0, 0, 25, 5, "#ff0000"));
+        shapes.add(new Oval(0, 0, 15, 5, "#ff0000"));
+        shapes.add(new Rectangle(0, 0, 8, 5, "#ff0000"));
+        shapes.add(new Rectangle(0, 0, 3, 4,"#00ff00"));
+        shapes.add(new Square(0, 0, 10, "#0000ff"));
+        shapes.add(new Triangle(1, 1, 4, 2, 3, 3, "#00ffff"));
         for (Shape s : shapes) {
             s.draw(null); // polymorphism! we have one line of code here that can call many different draw() methods depending on the specific shape s.
         }
